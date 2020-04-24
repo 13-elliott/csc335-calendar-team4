054ec0727b9c7d0ae6580480e483643d126c47d2
Molly Opheim <mollyopheim@gmail.com>
2020-04-24 15:32:30 -0700

    create event stuff

--- src/controller/CalendarController.java
+++ src/controller/CalendarController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import java.util.Date;
+import model.CalendarEvent;
+
 public class CalendarController {
+	
+	public CalendarController() {
+		
+	}
+	
+	
+	/**
+	 * Creates a CalendarEvent object to represent a calendar event
+	 * @param title -- event title
+	 * @param date -- event date
+	 * @param startTime -- event start time
+	 * @param endTime -- event end time
+	 * @param location -- event location
+	 * @param notes -- notes about the event
+	 * @return the CalendarEvent object created
+	 */
+	public CalendarEvent createEvent(String title, Date date, Date startTime, Date endTime, String location, String notes) {
+		CalendarEvent event = new CalendarEvent(title, date, startTime, endTime, location, notes);
+		return event;
+	}
+	
+	
+	
 }
