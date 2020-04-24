0eb6329a79bad8d78c06c970eab890dd25748b6f
Jessica Coan <jcoan188@gmail.com>
2020-04-23 19:47:00 -0700

    files i think

---
+++ src/Calendar.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+import view.CalendarView;
+
+public class Calendar {
+    public static void main(String[] args) {
+        Application.launch(CalendarView.class, args);
+    }
+}
