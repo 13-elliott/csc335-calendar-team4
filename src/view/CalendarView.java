5b72f60fa001bc282e1de9791feae2d46060f354
Kitty Elliott <kitty@glitchcat.space>
2020-04-29 23:30:34 -0700

    added menu items and functionality to add new events and calendars

--- src/view/CalendarView.java
+++ src/view/CalendarView.java
@@ -1,12 +1,13 @@
 package view;
 
+import controller.CalendarAlreadyExistsException;
 import controller.CalendarController;
+import controller.NoSuchCalendarException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -36,20 +37,31 @@ public class CalendarView extends Application {
 
         current = month;
 
-        Menu m = new Menu("View");
+        mainColumn = new VBox(constructMenus(), current.getNode());
+
+        stage.setTitle("Calendar");
+        stage.setScene(new Scene(mainColumn));
+        stage.show();
+    }
+
+    private MenuBar constructMenus() {
+        Menu viewMenu = new Menu("View");
         MenuItem monthItem = new MenuItem("Month");
         MenuItem weekItem = new MenuItem("Week");
         MenuItem dayItem = new MenuItem("Day");
         monthItem.setOnAction(e -> switchTo(month));
         weekItem.setOnAction(e -> switchTo(week));
         dayItem.setOnAction(e -> switchTo(day));
-        m.getItems().addAll(monthItem, dayItem, weekItem);
+        viewMenu.getItems().addAll(monthItem, dayItem, weekItem);
 
-        mainColumn = new VBox(new MenuBar(m), current.getNode());
+        Menu createMenu = new Menu("Create");
+        MenuItem createEventItem = new MenuItem("New Event");
+        createEventItem.setOnAction(this::createEvent);
+        MenuItem createCalItem = new MenuItem("New Calendar");
+        createCalItem.setOnAction(this::createCalendar);
+        createMenu.getItems().addAll(createEventItem, createCalItem);
 
-        stage.setTitle("Calendar");
-        stage.setScene(new Scene(mainColumn));
-        stage.show();
+        return new MenuBar(viewMenu, createMenu);
     }
 
     /**
@@ -67,5 +79,46 @@ public class CalendarView extends Application {
         children.add(target.getNode());
         current = target;
         stage.sizeToScene();
+    }
+
+    private void createCalendar(ActionEvent e) {
+        TextInputDialog in = new TextInputDialog();
+        in.setTitle("Name Calendar");
+        in.setHeaderText("Please enter a name for the new calendar");
+        // input validation:
+        in.getDialogPane().lookupButton(ButtonType.OK)
+                .addEventFilter(ActionEvent.ACTION, okEvent -> {
+                    // if given an existing calendar name,
+                    // show an error and don't close the dialog
+                    if (controller.getCalendarNames()
+                            .contains(in.getEditor().getText())) {
+                        new Alert(Alert.AlertType.ERROR,
+                                "A calendar with that name already exists")
+                                .showAndWait();
+                        okEvent.consume();
+                    }
+                });
+        in.showAndWait().ifPresent(newName -> {
+            try {
+                controller.createNewCalendar(newName);
+            } catch (CalendarAlreadyExistsException ex) {
+                // handled by event filter above: should not occur
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    private void createEvent(ActionEvent e) {
+        EventDialog.newEvent(controller.getCalendarNames())
+                .showAndWait()
+                .ifPresent(p -> {
+                    try {
+                        controller.addEvent(p.getKey(), p.getValue());
+                        // referesh the current view
+                        current.setDate(current.getDate());
+                    } catch (NoSuchCalendarException ex) {
+                        ex.printStackTrace();
+                    }
+                });
     }
 }
